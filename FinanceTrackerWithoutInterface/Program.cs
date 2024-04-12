611b399c868dbaf39a11be43d562f1831182fb02
Erik Mustafaev <toteshnell@outlook.com>
2024-04-12 18:23:28 +0300

    остался WinForm

--- FinanceTrackerWithoutInterface/Program.cs
+++ FinanceTrackerWithoutInterface/Program.cs
@@ -26,7 +26,67 @@ namespace FinanceTrackerWithoutInterface
 
         public void setIncome(double newIncome)
         {
-
+            income = newIncome;
         }
+
+        public void addTransaction(string category, double amount)
+        {
+            double total = calculateTotal();
+            if (total + amount > income)
+            {
+                showNotification("Сумма транзакции не может превышать доход");
+                return;
+            }
+            transactions.Add(new Transaction(category, amount));
+        }
+
+        public double calculateTotal()
+        {
+            double total = 0;
+            foreach (var transaction in transactions)
+            {
+                total += transaction.amount;
+            }
+            return total;
+        }
+        public List<Transaction> getTransaction()
+        {
+            return transactions;
+        }
+        public double getIncome()
+        {
+            return income;
+        }
+
+        public void addCategory(string category)
+        {
+            if(category!= "Общие" && category != "Одиночные")
+            {
+                categories.Add(category);
+            }
+        }
+        public List<string> getCategories()
+        {
+            return categories;
+        }
+
+        public void showNotification(string message)
+        {
+            MessageBox.Show(message, "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+    public class MainForm : Form
+    {
+
     }
 }
